8b6955a7eebad0ea62561a5b1ed6d0dfac8ae4a2
Chris Anastasi <canastasi@schoolone.education.local>
2019-01-14 16:24:06 -0500

    created folder for header. implemented css modules.

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,30 +1,13 @@
 import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 import React from 'react'
+import styles from './header.module.css'
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: `black`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-            fontSize: `25px`
-          }}
-        >
+  <div className={styles.background}>
+    <div className={styles.siteTitle}>
+      <h1>
+        <Link to="/" className={styles.link}>
           {siteTitle}
         </Link>
       </h1>
@@ -42,7 +25,6 @@ const Header = ({ siteTitle }) => (
           Slug needed here
         </Link>
       </h2> */}
-
     </div>
   </div>
 
