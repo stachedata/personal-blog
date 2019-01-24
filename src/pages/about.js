9bb1eabd6c35a9909442968adb90ee5cac4fcc4f
Chris Anastasi <stachedata@gmail.com>
2019-01-23 20:51:52 -0500

    created "about" page

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => (
+  <Layout>
+    <p>Welcome to About page</p>
+  </Layout>
+)
+
+export default AboutPage
