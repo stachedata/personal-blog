dec74ca8d936693f895e667e869c11591fa59ef6
Chris Anastasi <stachedata@gmail.com>
2019-01-24 21:39:04 -0500

    removed header and metaTitle query

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,40 +1,23 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { StaticQuery, graphql } from 'gatsby'
-
-import Header from './header/header'
 import './layout.css'
 
 const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div>
-         {children}
-        </div>
-        <footer 
-          style={{
-            position: `fixed`,
-            bottom: 0,
-            margin: 10
-          }}>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-      </>
-    )}
-  />
+  <>
+    <div>
+      {children}
+    </div>
+    <footer 
+      style={{
+        position: `fixed`,
+        bottom: 0,
+        margin: 10
+      }}>
+        © {new Date().getFullYear()}, Built with
+        {` `}
+        <a href="https://www.gatsbyjs.org">Gatsby</a>
+      </footer>
+  </>
 )
 
 Layout.propTypes = {
