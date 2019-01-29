eb45780d805442a2fe5d2525a38530b4b4969ad9
Chris Anastasi <stachedata@gmail.com>
2019-01-28 20:57:36 -0500

    added header

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Layout from '../components/layout'
+import Header from '../components/header/header'
 
 const AboutPage = () => (
-  <Layout>
-    <p>Welcome to About page</p>
-  </Layout>
+  <>
+    <Header></Header>
+    <Layout>
+      <p>Welcome to About page</p>
+    </Layout>
+  </>
 )
 
 export default AboutPage
